b0502d6f999bc09599f722a12256e1f0d6c3eebf
insidemirage <vladislavlightinthedarkness@gmail.com>
2020-02-19 22:54:46 +0300

    somechanges in manage.py

--- manage.py
+++ manage.py
@@ -5,6 +5,11 @@ import json
 import sqlite3
 
 
+# TODO configure
+def configure_query(delimiter=",",type="str", **kwargs) -> str:
+    pass
+
+
 def get_config() -> dict:
     print("Loading settings.json file")
     path = f"{get_folder()}/settings.json"
@@ -76,13 +81,67 @@ def add_user_to_db(username, password, status) -> bool:
     return True
 
 
-def remove_user_from_bd() -> bool:
-    pass
+def remove_user_from_bd(username = None, id=None) -> bool:
+    """Removes user from db
+
+    :param username: -- name of the user
+    :param id: -- id of the user
+    :return: True/False
+    """
+    query = "DELETE FROM users WHERE"
+    if username and id:
+        query += f" username='{username}' AND id={id}"
+    elif username:
+        query += f" username='{username}'"
+    elif id:
+        query += f" id={id}"
+    else:
+        print("Need args")
+        return False
+    print(query)
+    dbname = get_config()["result"]["database"]
+    connection = connect_db(dbname)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    cursor.execute("SELECT * FROM users")
+    print(cursor.fetchall())
+    return True
 
 
-def change_user_status() -> bool:
-    pass
+def change_user_settings(**keys) -> bool:
+    """Changes user info
 
+    :param username: - user login
+    :param key: -- user password
+    :param status: -- 0 or 1 | user or admin
+    :return:
+    """
+    query = "UPDATE users SET "
+    for i, values in enumerate(keys):
+        if i == 0 and keys[values] is not None:
+            query += f"{values}='{keys[values]}'"
+            print("here")
+            continue
+        elif i != 0:
+            query += f","
+        if values == "status":
+            if int(keys[values]) != 1 or int(keys[values]) != 0:
+                return False
+            else:
+                query += f"{values}={keys[values]}"
+        elif keys[values]:
+            query += f"{values}='{keys[values]}'"
+            print("ti kek realna")
+    print(query)
+    # dbname = get_config()["result"]["database"]
+    # connection = connect_db(dbname)
+    # cursor = connection.cursor()
+    # cursor.execute(query)
+    # connection.commit()
+    # cursor.execute("SELECT * FROM users")
+    # print(cursor.fetchall())
+    return True
 
 def connect_db(dbname) -> sqlite3.connect:
     """Connection to Database function
@@ -206,11 +265,24 @@ def create_parser() -> None:
     """
     Users
     """
-    uuu = subparsers.add_parser("add_user")
-    uuu.add_argument("username")
-    uuu.add_argument("password")
-    uuu.add_argument("status")
-    uuu.set_defaults(add_user=add_user_to_db)
+    users = dict()
+    # Add user
+    users["add"] = subparsers.add_parser("add_user")
+    users["add"].add_argument("username")
+    users["add"].add_argument("password")
+    users["add"].add_argument("status")
+    users["add"].set_defaults(add_user=add_user_to_db)
+    # Remove user
+    users["remove"] = subparsers.add_parser("remove_user")
+    users["remove"].add_argument("--username", dest="removeuname")
+    users["remove"].add_argument("--id", dest="removeid")
+    users["remove"].set_defaults(remove_user=True)
+    # Change user info
+    users["change"] = subparsers.add_parser("change_user")
+    users["change"].add_argument("--username", dest="change_name")
+    users["change"].add_argument("--password", dest="change_pass")
+    users["change"].add_argument("--status", dest="change_status")
+    users["change"].set_defaults(change_user=True)
     args = parser.parse_args()
 
     # TODO don't pass function to the args or call it from args
@@ -218,7 +290,11 @@ def create_parser() -> None:
         setup_handler()
     elif "add_user" in args:
         add_user_to_db(args.username, args.password, args.status)
-
+    elif "remove_user" in args:
+        print(args)
+        remove_user_from_bd(username=args.removeuname, id=args.removeid)
+    elif "change_user" in args:
+        change_user_settings(username=args.change_name, password=args.change_pass, status=args.change_status)
 
 if __name__ == "__main__":
     create_parser()
